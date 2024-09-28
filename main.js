361318f1c5c09102b71e74ca0290ffab00289cd2
pitbull90 <henrycordero@live.com>
2024-09-27 20:16:49 -0700

    Started js validation with the Name input

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log(document);
+const createAccountForm = document.getElementById('createAccountForm'),
+fullname = document.getElementById('fullname'),
+email = document.getElementById('email'),
+username = document.getElementById('username'),
+password = document.getElementById('password'),
+repeatPassword = document.getElementById('repeat-password');
+
+createAccountForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    ValidateForm();
+})
+
+function ValidateForm() {
+    if (CheckFullName()) {
+        alert('Valid');
+    }
+}
+function CheckFullName() {
+    const fullNameValue = fullname.value.trim();
+    if (fullNameValue === '') {
+        alert('Please Enter Full Name');
+        return false;
+    } else {
+        return true;
+    }
+    
+}
+function CheckEmail() {
+    
+}
+function CheckPassword() {
+    
+}
+function CheckReapeatPassword() {
+    
+}
